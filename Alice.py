0de7dcadad767bb4f664dbbfe75783bacd338350
Gonzalo <gonzaloabalgonzalez@gmail.com>
2022-01-13 23:48:27 +0100

    Implemented MQTT but need to finish app

--- Alice.py
+++ Alice.py
@@ -1,37 +1,61 @@
+import os
+from user import User
 import mqtt_utils, time
-from cryptography.hazmat.primitives import hashes, hmac
-from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey, X25519PublicKey
 
-# clientAlice = mqtt_utils.connect_mqtt("Alice_acb2022")
-# mqtt_utils.publish(clientAlice, "ALICEACB.out", "Hola Bob")
-# mqtt_utils.subscribe(clientAlice, "BOBACB.in")
-# clientAlice.loop_start()
 
-# while True:
-#     time.sleep(100)
+def on_message(client, userdata, msg):
+    if not flag:
+        Alice.state.diffieHellman_remote = X25519PublicKey.from_public_bytes(msg.payload)
+        msg = Alice.state.public_key.public_bytes(encoding=serialization.Encoding.Raw,
+                                                             format=serialization.PublicFormat.Raw)
+        mqtt_utils.publish(clientAlice, "ACB.out", msg)
+        flag = True
+        return 
+        # Get Bob's public key
+        # Send Alice's public key
+    print(chr(27)+"[1;31m" + "\n\nBob:")
+    print(msg.payload)
+    print(Bob.receive(msg.payload))
+    print(chr(27)+"[1;35m" + "\n\nAlice:")
+
+if __name__ == "__main__":
+    flag = False
+    # Generating the first shared root key of 32 bytes
+    initial_root_key = b"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" 
+
+    # Initializing users that are going to communicate
+    Alice = User("Alice", initial_root_key)
+    # Bob = User("Bob", initial_root_key)
+
+    # Initialize ratchets
+    # Alice.initialize_ratchet(Bob.state.public_key)
+    # Bob.initialize_ratchet(Alice.state.public_key)
     
-root_key = b""
-
-alice_private_dh = X25519PrivateKey.generate()
-alice_public_dh = alice_private_dh.public_key()
-
-bob_private_dh = X25519PrivateKey.generate()
-bob_public_dh = bob_private_dh.public_key()
-
-shared_key1 = alice_private_dh.exchange(bob_public_dh)
-shared_key2 = bob_private_dh.exchange(alice_public_dh)
+    clientAlice = mqtt_utils.connect_mqtt("ALICE")
+    mqtt_utils.subscribe(clientAlice, "ACB.in")
+    clientAlice.on_message = on_message
+    clientAlice.loop_start()
+    time.sleep(1)
     
-root_key, chain_key = HKDF(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=root_key,
-    info=b'handshake data',
-).derive(shared_key1)
-
-hash = hmac.HMAC(chain_key, hashes.SHA256)
-hash.update(b"")
+    while True: 
+        value = input(chr(27)+"[1;35m"+"\n\nAlice:")
+        print(chr(27)+"[0;30m")
+        msg = Alice.send(value)
+        mqtt_utils.publish(clientAlice, "ACB.out", msg)
+        
 
 
-    
-    
+
+    # # Now both have the same root key and the shared secret
+    # b_msg_1 = Bob.send("Hello world!")
+    # print(Alice.receive(b_msg_1))
+    # b_msg_2 = Bob.send("This is Bob's second message")
+    # print(Alice.receive(b_msg_2))
+    # a_msg_1 = Alice.send("Hello, I'm Alice!")
+    # print(Bob.receive(a_msg_1))
+    # b_msg_3 = Bob.send("Nice to meet you Alice!, I'm Bob")
+    # print(Alice.receive(b_msg_3))
+    # # TODO Instead of local communication use the MQTT Server
+    # # TODO Implement the logic to send/receive messages
--- Bob.py
+++ Bob.py
@@ -1,8 +1,54 @@
+import os
+from user import User
 import mqtt_utils, time
 
-clientBob = mqtt_utils.connect_mqtt("Bob_2022")
-mqtt_utils.publish(clientBob, "BOBACB.in", "Hola Alice")
-mqtt_utils.subscribe(clientBob, "ALICEACB.out")
-clientBob.loop_start()
-while True:
-    time.sleep(100)
+def on_message(client, userdata, msg):
+    if not flag:
+        print()
+        # Get Bob's public key
+        # Send Alice's public key
+    print(chr(27)+"[1;31m" + "\n\nAlice:")
+    print(msg.payload)
+    print(Bob.receive(msg.payload))
+    print(chr(27)+"[1;35m" + "\n\nBob:")
+
+if __name__ == "__main__":
+    flag = False
+    # Generating the first shared root key of 32 bytes
+    initial_root_key = b"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" 
+
+    # Initializing users that are going to communicate
+    Alice = User("Alice", initial_root_key)
+    Bob = User("Bob", initial_root_key)
+
+    # Initialize ratchets
+    Alice.initialize_ratchet(Bob.state.public_key)
+    Bob.initialize_ratchet(Alice.state.public_key)
+
+
+    clientBob = mqtt_utils.connect_mqtt("BOB")
+    mqtt_utils.subscribe(clientBob, "ACB.out")
+    clientBob.on_message = on_message
+    clientBob.loop_start()
+    time.sleep(1)
+
+    while True:
+        value = input(chr(27)+"[1;35m"+"\n\nBob:")
+        print(chr(27)+"[1;30m")
+        msg = Bob.send(value)
+        mqtt_utils.publish(clientBob, "ACB.in", msg)
+
+
+
+
+    # # Now both have the same root key and the shared secret
+    # b_msg_1 = Bob.send("Hello world!")
+    # print(Alice.receive(b_msg_1))
+    # b_msg_2 = Bob.send("This is Bob's second message")
+    # print(Alice.receive(b_msg_2))
+    # a_msg_1 = Alice.send("Hello, I'm Alice!")
+    # print(Bob.receive(a_msg_1))
+    # b_msg_3 = Bob.send("Nice to meet you Alice!, I'm Bob")
+    # print(Alice.receive(b_msg_3))
+    # # TODO Instead of local communication use the MQTT Server
+    # # TODO Implement the logic to send/receive messages
